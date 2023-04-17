06494c3731df8a09ddf98c8e74139dd31f8aa927
h602121 <74725234+h602121@users.noreply.github.com>
2023-04-17 12:24:51 +0200

    Laget metode for utregning av snittscore på alle stands. Mangler litt for å gjøre metode ferdig

---
+++ src/main/java/com/example/expo/StandScoreAvg.java
@@ -0,0 +1,60 @@
+package com.example.expo;
+
+import com.example.expo.Model.Entity.Stand;
+
+public class StandScoreAvg {
+    private Stand stand;
+    private double posterAvg;
+    private double presentationAvg;
+    private double contentAvg;
+    private double totalAvg;
+
+    public StandScoreAvg(Stand stand, double posterAvg, double presentationAvg, double contentAvg, double totalAvg) {
+        this.stand = stand;
+        this.posterAvg = posterAvg;
+        this.presentationAvg = presentationAvg;
+        this.contentAvg = contentAvg;
+        this.totalAvg = totalAvg;
+    }
+
+    public Stand getStand() {
+        return stand;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
+    }
+
+    public double getPosterAvg() {
+        return posterAvg;
+    }
+
+    public void setPosterAvg(double posterAvg) {
+        this.posterAvg = posterAvg;
+    }
+
+    public double getPresentationAvg() {
+        return presentationAvg;
+    }
+
+    public void setPresentationAvg(double presentationAvg) {
+        this.presentationAvg = presentationAvg;
+    }
+
+    public double getContentAvg() {
+        return contentAvg;
+    }
+
+    public void setContentAvg(double contentAvg) {
+        this.contentAvg = contentAvg;
+    }
+
+    public double getTotalAvg() {
+        return totalAvg;
+    }
+
+    public void setTotalAvg(double totalAvg) {
+        this.totalAvg = totalAvg;
+    }
+}
+
